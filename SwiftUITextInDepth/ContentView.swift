73da8e163812773d38476618171d48cdaa03b2fe
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-01-19 16:43:45 +0200

    Initial Commit

---
+++ SwiftUITextInDepth/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUITextInDepth
+//
+//  Created by Ramill Ibragimov on 19.01.2020.
+//  Copyright © 2020 Ramill Ibragimov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
